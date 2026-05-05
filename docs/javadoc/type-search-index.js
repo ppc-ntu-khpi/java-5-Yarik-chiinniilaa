456e30e0201e7daaf7c3e82b8e8f03d6266ff427
Ярослав Полиба - chiinniilaa <Tomatusik1@ukr.net>
2026-05-05 13:18:16 +0300

    Delete docs/javadoc directory

--- docs/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"domain","l":"Artist"},{"p":"domain","l":"Editor"},{"p":"domain","l":"Employee"},{"p":"domain","l":"GraphicIllustrator"},{"p":"domain","l":"Manager"},{"p":"domain","l":"TechnicalWriter"}];updateSearchResults();
